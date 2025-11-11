8184f300d1fc8fe58f13397e6250f08206f35003
Hasan Ablak <0hasanablak@gmail.com>
2025-11-11 16:50:43 +0300

    Update videos-grid.blade.php

--- resources/views/components/videos-grid.blade.php
+++ resources/views/components/videos-grid.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 	@forelse($videos as $video)
 		<x-video :video="$video" />
 	@empty
